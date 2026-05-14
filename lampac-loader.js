0cb9ac623c5a3789d89bb85ceb6f5314c091b5e4
vaservaga <vaservaga@gmail.com>
2026-05-14 12:53:41 +0300

    Add files via upload

---
+++ lampac-loader.js
@@ -0,0 +1,6 @@
+(function () {
+    var script = document.createElement('script');
+    script.src = 'https://192.168.0.3:9443/online.js';
+    script.async = false;
+    document.head.appendChild(script);
+})();
