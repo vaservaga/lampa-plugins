cf7d41fb0caaa152fb0a89506208e0bc706be107
vaservaga <vaservaga@gmail.com>
2026-05-14 17:10:17 +0300

    Update lampac-loader.js

--- lampac-loader.js
+++ lampac-loader.js
@@ -1,6 +1,17 @@
 (function () {
-    var script = document.createElement('script');
-    script.src = 'https://192.168.0.3:9443/online.js';
-    script.async = false;
-    document.head.appendChild(script);
+
+    var plugins = [
+        'https://vaservaga.github.io/lampa-plugins/plugins/tmdb_proxy/tmdb_proxy.js',
+        'https://vaservaga.github.io/lampa-plugins/plugins/online/online.js',
+        'https://vaservaga.github.io/lampa-plugins/plugins/collections/collections.js',
+        'https://vaservaga.github.io/lampa-plugins/plugins/tracks/tracks.js'
+    ];
+
+    plugins.forEach(function(url) {
+        var script = document.createElement('script');
+        script.src = url;
+        script.async = false;
+        document.head.appendChild(script);
+    });
+
 })();
